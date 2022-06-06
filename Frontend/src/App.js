eaf8546978d5b5dcd51a47bf940528d404bfb146
fplanting <frida.planting@hotmail.com>
2022-06-06 22:30:12 +0200

    fixed some styling

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import CreateDocument from "./pages/CreateDocument";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="navbar">
+        <div className="links">
+          <a href="/mainpage">Main Page</a>
+          <a href="/createpost">Create Post</a>
+        </div>
+      </div>
+
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path="/CreateDocument" element={<CreateDocument />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ Frontend/src/pages/CreateDocument.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CreateDocument() {
+  return (
+    <div className="CreateDocument">
+      <h1>CreateDocument</h1>
+    </div>
+  );
+}
